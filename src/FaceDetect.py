4693b7e6b8ec5e11bdf6ebec4ab05a77ae0d7d65
Richard Mao <richard.mao.123@gmail.com>
2017-07-03 08:33:28 -0700

    updated facedetect

--- src/FaceDetect.py
+++ src/FaceDetect.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
-
+#face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
+face_cascade = cv2.CascadeClassifier('/home/richard/OpenCV/data/haarcascades/haarcascade_frontalface_alt.xml')
 while(1):
 
 	ret,frame = cap.read()
 	
-	gray = cv2.cvtColo(frame,cv2.Color_BGR2GRAY)
+	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, 1.1,5)
 
 	for(x,y,w,h) in faces:
